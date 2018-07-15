be4d2a08f8e24de423b2b6b38690ec72817ce6bd
andrzejmara <andrzejmara@gmail.com>
2018-07-15 16:42:32 +0200

    Polished the UI

--- 1_Warsztaty/1_Manager/Program.cs
+++ 1_Warsztaty/1_Manager/Program.cs
@@ -10,9 +10,21 @@ namespace _1_Manager
 		{
 			List<TaskModel> tasksList = new List<TaskModel>();
 
+			ConsoleEx.WriteLine("Hi! This is a program that lets you organize your Task List!", ConsoleColor.Green);
+			var options = new List<string>() { "add", "delete", "show", "save", "load" };
+			ConsoleEx.WriteLine("This is the list of available commands: ",ConsoleColor.Green);
+			foreach (var option in options)
+			{
+				Console.WriteLine(option);
+			}
+			Console.WriteLine();
+
+
+
+
 			string command;
 			do
-			{
+			{	
 				ConsoleEx.WriteLine("Write your command: ", ConsoleColor.Yellow);
 				command = Console.ReadLine();
 				command = command.Trim().ToLower();
@@ -21,8 +33,8 @@ namespace _1_Manager
 				if (command == "add")
 				{
 					tasksList.Add(TaskAdder.AddTask());
-					
-					
+
+
 
 					Console.WriteLine("Added");
 
@@ -37,8 +49,8 @@ namespace _1_Manager
 				{
 					//Orders list by importance and date
 					var tasksListOrdered = from t in tasksList
-						orderby t._important descending, t._startDate descending
-						select t;
+										   orderby t._important descending, t._startDate descending
+										   select t;
 					tasksList = tasksListOrdered.ToList();
 
 					TaskShow.TaskShower(tasksList);
--- 1_Warsztaty/1_Manager/TaskListLoader.cs
+++ 1_Warsztaty/1_Manager/TaskListLoader.cs
@@ -10,34 +10,46 @@ namespace _1_Manager
 	{
 		public static List<TaskModel> Load(List<TaskModel> tasksList)
 		{
-			string filePath = $"{Directory.GetCurrentDirectory()}\\Data.csv";
 
-			List<string> newListOfData = File.ReadAllLines(filePath).ToList();
+		string filePath = $"{Directory.GetCurrentDirectory()}\\Data.csv";
 
-			foreach (var line in newListOfData)
+			
+
+			if (File.Exists(filePath))
 			{
 
-				string[] entries = line.Split(',');
-
-
-				if (entries[2] == "")
+				List<string> newListOfData = File.ReadAllLines(filePath).ToList();
+				foreach (var line in newListOfData)
 				{
-					tasksList.Add(new TaskModel(entries[0],
-						DateTime.ParseExact(entries[1], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture), null,
-						bool.Parse(entries[3]), bool.Parse(entries[4])));
+
+					string[] entries = line.Split(',');
+
+
+					if (entries[2] == "")
+					{
+						tasksList.Add(new TaskModel(entries[0],
+							DateTime.ParseExact(entries[1], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture), null,
+							bool.Parse(entries[3]), bool.Parse(entries[4])));
+					}
+					else if (entries[2] != "")
+					{
+						tasksList.Add(new TaskModel(entries[0],
+							DateTime.ParseExact(entries[1], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture), DateTime.ParseExact(entries[2], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture),
+							bool.Parse(entries[3]), bool.Parse(entries[4])));
+					}
+
+
+
+
+
 				}
-				else if (entries[2] != "")
-				{
-					tasksList.Add(new TaskModel(entries[0],
-						DateTime.ParseExact(entries[1], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture), DateTime.ParseExact(entries[2], "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture),
-						bool.Parse(entries[3]), bool.Parse(entries[4])));
-				}
-
-
-
-
-
 			}
+			else
+			{
+				Console.WriteLine("There is no Data file ;(");
+			}
+
+		
 
 			return tasksList;
 
